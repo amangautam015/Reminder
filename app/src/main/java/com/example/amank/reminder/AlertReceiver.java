08e444e722440ddc7f3c2104702b6c32a57f2cf4
Aman <13annon@gmail.com>
2017-08-06 07:34:37 +0530

    Debugged Deletion of Reminder

---
+++ app/src/main/java/com/example/amank/reminder/AlertReceiver.java
@@ -0,0 +1,44 @@
+package com.example.amank.reminder;
+/*
+* Designed By AmanK
+* */
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+public class AlertReceiver extends BroadcastReceiver {
+    String name1 ;
+    int itd;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        itd = intent.getIntExtra("EXTRA_SESSION_ID",0);
+        name1 = intent.getStringExtra("MESSAGE");
+        Log.e("TAGGG",""+itd);
+        Log.e("TAGGG2",name1);
+        createNotification(context, name1,"Your Event is about to begin please report Asap,Thankyou", "Alert",itd);
+
+
+    }
+
+    public void createNotification(Context context, String msg, String msgText, String msgAlert,int itd) {
+
+        PendingIntent notificationIntent = PendingIntent.getActivity(context, itd,
+                new Intent(context, CatalogActivity.class), itd);
+        NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(msg)
+                .setTicker(msgAlert)
+                .setContentText(msgText);
+        mBuilder.setContentIntent(notificationIntent);
+        mBuilder.setDefaults(NotificationCompat.DEFAULT_VIBRATE);
+        //mBuilder.setAutoCancel(true);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(itd, mBuilder.build());
+    }
+}
